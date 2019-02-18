0399d13fc1f17f7a6077f833eeb40de60f248ac5
IvanBushtin <ivanbushtin77@gmail.com>
2019-02-18 18:44:23 +0200

    add header menu

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,11 @@
-$(".burger").on("click", function () {
-    $(".nav").toggle();
+document.addEventListener('DOMContentLoaded', function () {
+    var hamburger = document.getElementById('openMenu');
+    var navList =  document.getElementsByClassName('hidden')[0];
+
+    hamburger.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        navList.classList.toggle('nav-list');
+        hamburger.classList.toggle('rotate');
+    });
 });
